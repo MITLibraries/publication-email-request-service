abe8125ae4e32f7b981f611c16343b5a939dca9c
jonavellecuerdo <cuerdojonavelle@gmail.com>
2024-05-01 10:28:00 -0400

    Add CLI main command

--- pers/cli.py
+++ pers/cli.py
@@ -4,25 +4,31 @@ from time import perf_counter
 
 import click
 
-from pers.config import configure_logger, configure_sentry
+from pers.config import Config
+from pers.database_engine import DatabaseEngine
+from pers.elements import ElementsClient
 
 logger = logging.getLogger(__name__)
+CONFIG = Config()
 
 
-@click.command()
+@click.group()
 @click.option(
-    "-v", "--verbose", is_flag=True, help="Pass to log at debug level instead of info"
+    "-v", "--verbose", help="Pass to log at debug level instead of info", is_flag=True
 )
-def main(*, verbose: bool) -> None:
-    start_time = perf_counter()
-    root_logger = logging.getLogger()
-    logger.info(configure_logger(root_logger, verbose=verbose))
-    logger.info(configure_sentry())
-    logger.info("Running process")
-
-    # Do things here!
-
-    elapsed_time = perf_counter() - start_time
-    logger.info(
-        "Total time to complete process: %s", str(timedelta(seconds=elapsed_time))
+@click.pass_context
+def main(ctx: click.Context, verbose: bool) -> None:
+    ctx.ensure_object(dict)
+    ctx.obj["START_TIME"] = perf_counter()
+    logger.info(CONFIG.configure_logger(verbose))
+    CONFIG.check_required_env_vars()
+    db_engine = DatabaseEngine.configure(CONFIG.PERS_DATABASE_CONNECTION_STRING)
+    elements_client = ElementsClient(
+        user=CONFIG.ELEMENTS_USER,
+        password=CONFIG.ELEMENTS_PASSWORD,
+        proxies={"https": CONFIG.QUOTAGUARD_STATIC_URL},
     )
+    elements_client.authenticate(url=CONFIG.ELEMENTS_ENDPOINT)
+    ctx.obj["db_engine"] = db_engine
+    ctx.obj["elements_client"] = elements_client
+    logger.info("Running process")
